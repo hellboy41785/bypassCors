bdaefd9dd6cbe04e9b02695bb0a8bdc5c94efd44
hellboy41785 <unknown41785@gmail.com>
2023-02-22 14:06:35 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -2,32 +2,34 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-// enable CORS
-app.use(cors());
-// set the port on which our app wil run
-// important to read from environment variable if deploying
-const port = process.env.PORT || 5000;
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// basic string route to prevent Glitch error
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+const corsProxy = createProxyMiddleware({
+  changeOrigin: true,
+  target: "https://www.nseindia.com", // this can be any target URL
 });
 
-// the route we're working with
-app.get('/corsbypass', async (req, res) => {
-    const { url } = req.query;
-  
-    try {
-      const response = await axios.get(url);
-      res.send(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error retrieving data');
-    }
-  });
+app.use('/corsbypass', corsProxy);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/corsbypass", async (req, res) => {
+  const { url } = req.query;
+
+  try {
+    const response = await axios.get(url);
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving data");
+  }
+});
 
 // console text when app is running
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-})
+  console.log(`Server listening at http://localhost:${port}`);
+});
